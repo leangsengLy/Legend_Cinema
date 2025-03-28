c67444a0760a2fb73eb6c71b613e9f930f320f9e
leangsengLy <edeepleang@gmail.com>
2025-03-28 07:12:27 +0700

    update

--- legend_cinema/lib/Pages/news.dart
+++ legend_cinema/lib/Pages/news.dart
@@ -16,70 +16,47 @@ class NewsState extends State<News> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height - 100,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: 200,
-                      decoration: const BoxDecoration(
-                        color: Colors.red,
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height - 100,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        height: 200,
+                        decoration: const BoxDecoration(
+                          color: Colors.red,
+                        ),
+                        child: Image.asset(
+                          "assets/Image/news/6.jpeg",
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                      child: Image.asset(
-                        "assets/Image/news/6.jpeg",
-                        fit: BoxFit.cover,
+                      const SizedBox(
+                        height: 20,
                       ),
-                    ),
-                    ...listNews.map(
-                      (val) {
-                        return Cardoffer(
-                          pathImage: val.UrlImage,
-                          description: val.description,
-                        );
-                      },
-                    ).toList(),
-
-                    // const Text(
-                    //   "What's we offer",
-                    //   style: TextStyle(
-                    //     color: Colors.white,
-                    //     fontSize: 17,
-                    //   ),
-                    // ),
-                    // const SizedBox(height: 10),
-                    // const SizedBox(height: 16),
-                    // const Cardoffer(
-                    //   pathImage: "assets/Image/offers/2.jpeg",
-                    // ),
-                    // const SizedBox(height: 16),
-                    // const Cardoffer(
-                    //   pathImage: "assets/Image/offers/3.jpeg",
-                    // ),
-                    // const SizedBox(height: 16),
-                    // const Cardoffer(
-                    //   pathImage: "assets/Image/offers/4.jpeg",
-                    // ),
-                    // const SizedBox(height: 16),
-                    // const Cardoffer(
-                    //   pathImage: "assets/Image/offers/5.jpeg",
-                    // ),
-                    // const SizedBox(height: 16),
-                    // const Cardoffer(
-                    //   pathImage: "assets/Image/offers/6.jpeg",
-                    // ),
-                  ],
+                      ...listNews.map(
+                        (val) {
+                          return Cardoffer(
+                            pathImage: val.UrlImage,
+                            description: val.description,
+                          );
+                        },
+                      ).toList(),
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ));
   }
 }
