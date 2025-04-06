5462ba5509269d071d4a2a85b4c9dfc5fd63f990
leangsengLy <edeepleang@gmail.com>
2025-04-06 23:05:59 +0700

    update login

--- legend_cinema/lib/Screen/Login/login.dart
+++ legend_cinema/lib/Screen/Login/login.dart
@@ -9,8 +9,72 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Login Screen"),
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Login",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 5),
+          const Text(
+            "You can use your phone number or username to login",
+            style: TextStyle(fontSize: 13),
+          ),
+          const SizedBox(height: 20),
+          const TextField(
+            decoration: InputDecoration(
+              labelText: "Username",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                borderSide: BorderSide(color: Colors.blue, width: 2),
+              ),
+              hintText: "Enter your username",
+            ),
+          ),
+          const SizedBox(height: 30),
+          const TextField(
+            keyboardType: TextInputType.visiblePassword,
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                borderSide: BorderSide(color: Colors.blue, width: 2),
+              ),
+              hintText: "Enter Password",
+              labelText: "Password",
+            ),
+          ),
+          const SizedBox(height: 20),
+          const Expanded(
+            child: SizedBox(
+              width: double.infinity,
+              child: Text(
+                textAlign: TextAlign.right,
+                "Forgot Password?",
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(50),
+            ),
+            alignment: Alignment.center,
+            child: const Text(
+              "Login",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
